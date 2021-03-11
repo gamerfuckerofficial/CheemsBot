fd1808cc0d4b8b22cc981e196c5ef5d4961b3968
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-03-11 15:02:48 +0530

    Update gmute_sql.py

--- userbot/modules/sql_helper/gmute_sql.py
+++ userbot/modules/sql_helper/gmute_sql.py
@@ -1,9 +1,9 @@
 try:
-    from userbot.modules.sql_helper import SESSION, BASE
+    from userbot.plugins.sql_helper import BASE, SESSION
 except ImportError:
-    raise AttributeError
+    raise Exception("Hello!")
 
-from sqlalchemy import Column, String, UnicodeText
+from sqlalchemy import Column, String
 
 
 class GMute(BASE):
