aec9341c6292af543fab7e268dbb9605ff443214
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-03-13 22:34:43 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -136,7 +136,7 @@ async def amireallyalive(alive):
     output = ("**𝕮𝖍𝖊𝖊𝖒𝖘𝕭𝖔𝖙 𝕴𝖟 𝕺𝖓𝖑𝖎𝖓𝖊** \n"
              f"**тєℓєтнση νєяѕιση: {version.__version__}** \n"
              f"**P̳y̳t̳h̳o̳n̳ ̳̳v̳e̳r̳s̳i̳o̳n̳🐍: {python_version()}** \n"
-             f"**𒆜ʍʏ օառɛʀ 𒆜: [{tag}](tg://user?id={user.id})** \n"
+             f"**𒆜ʍʏ օառɛʀ 𒆜: {DEFAULTUSER}** \n"
              f"**𝐑𝐄𝐏𝐎: [ĆĦ€€ΜŞβØŦ](https://github.com/gamerfuckerofficial/CheemsBot)** \n"
              f"**ᑕᕼᗩᑎᑎᗴᒪ: [𝕁𝕆𝕀ℕ](https://t.me/CheemsUserbot)**")
     if ALIVE_LOGO:
