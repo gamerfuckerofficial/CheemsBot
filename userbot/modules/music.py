3fb71ba4798c8677ece977a4ae021a79fe68888e
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-06-03 19:17:00 +0530

    Update music.py

--- userbot/modules/music.py
+++ userbot/modules/music.py
@@ -1,79 +1,147 @@
-# AnggaR96s
+# U S Σ R Δ T O R / Ümüd
 
-from telethon import events
-import subprocess
-from telethon.errors.rpcerrorlist import YouBlockedUserError
+import datetime
 import asyncio
-from userbot.events import register
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot import bot, CMD_HELP
-import glob
+from userbot.events import register
 import os
+import subprocess
+import glob
+from random import randint
+from userbot.cmdhelp import CmdHelp
 
-os.system("rm -rf *.mp3")
+# ██████ LANGUAGE CONSTANTS ██████ #
 
+from userbot.language import get_value
+LANG = get_value("song")
 
-def bruh(name):
-    os.system("instantmusic -q -s " + name)
+# ████████████████████████████████ #
 
-
-@register(outgoing=True, pattern=r"^.song (.*)")
-async def _(event):
+@register(outgoing=True, pattern="^.deez(\d*|)(?: |$)(.*)")
+async def deezl(event):
     if event.fwd_from:
         return
+    sira = event.pattern_match.group(1)
+    if sira == '':
+        sira = 0
+    else:
+        sira = int(sira)
+
+    sarki = event.pattern_match.group(2)
+    if len(sarki) < 1:
+        if event.is_reply:
+            sarki = await event.get_reply_message().text
+        else:
+            await event.edit(LANG['GIVE_ME_SONG']) 
+
+    await event.edit(LANG['SEARCHING'])
+    chat = "@DeezerMusicBot"
+    async with bot.conversation(chat) as conv:
+        try:     
+            mesaj = await conv.send_message(str(randint(31,62)))
+            sarkilar = await conv.get_response()
+            await mesaj.edit(sarki)
+            sarkilar = await conv.get_response()
+        except YouBlockedUserError:
+            await event.reply(LANG['BLOCKED_DEEZER'])
+            return
+        await event.client.send_read_acknowledge(conv.chat_id)
+        if sarkilar.audio:
+            await event.client.send_read_acknowledge(conv.chat_id)
+            await event.client.send_message(event.chat_id, LANG['UPLOADED_WITH'], file=sarkilar.message)
+            await event.delete()
+        elif sarkilar.buttons[0][0].text == "No results":
+            await event.edit(LANG['NOT_FOUND'])
+        else:
+            await sarkilar.click(sira)
+            sarki = await conv.wait_event(events.NewMessage(incoming=True,from_users=595898211))
+            await event.client.send_read_acknowledge(conv.chat_id)
+            await event.client.send_message(event.chat_id, f"`{sarkilar.buttons[sira][0].text}` | " + LANG['UPLOADED_WITH'], file=sarki.message)
+            await event.delete()
+
+@register(outgoing=True, pattern="^.song(?: |$)(.*)")
+async def port_song(event):
+    if event.fwd_from:
+        return
+    
     cmd = event.pattern_match.group(1)
+    if len(cmd) < 1:
+        await event.edit(LANG['UPLOADED_WITH']) 
+
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.edit("`Ok finding the song..`")
-    bruh(str(cmd))
+        
+    await event.edit(LANG['SEARCHING_SPOT'])  
+    dosya = os.getcwd() 
+    os.system(f"spotdl --song {cmd} -f {dosya}")
+    await event.edit(LANG['DOWNLOADED'])    
+
     l = glob.glob("*.mp3")
-    loa = l[0]
-    await event.edit("`Sending song..`")
-    await event.client.send_file(
-        event.chat_id,
-        loa,
-        force_document=True,
-        allow_cache=False,
-        caption=cmd,
-        reply_to=reply_to_id,
-    )
+    if len(l) >= 1:
+        await event.edit(LANG['UPLOADING'])
+        await event.client.send_file(
+            event.chat_id,
+            l[0],
+            force_document=True,
+            allow_cache=False,
+            reply_to=reply_to_id
+        )
+        await event.delete()
+    else:
+        await event.edit(LANG['NOT_FOUND'])   
+        return 
     os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3", shell=True)
-    await event.delete()
+    subprocess.check_output("rm -rf *.mp3",shell=True)
 
-
-@register(outgoing=True, pattern="^.smd(?: |$)(.*)")
-async def _(event):
+@register(outgoing=True, pattern="^.songpl ?(.*)")
+async def songpl(event):
     if event.fwd_from:
         return
-    link = event.pattern_match.group(1)
-    chat = "@SpotifyMusicDownloaderBot"
-    await event.edit("```Getting Your Music```")
-    async with bot.conversation(chat) as conv:
-        await asyncio.sleep(2)
-        await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=752979930)
-            )
-            await bot.send_message(chat, link)
-            respond = await response
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.reply(
-                "```Please unblock @SpotifyMusicDownloaderBot and try again```"
-            )
-            return
-        await event.delete()
-        await bot.forward_messages(event.chat_id, respond.message)
-        await bot.send_read_acknowledge(event.chat_id)
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+    cmd = event.pattern_match.group(1)
 
+    if len(cmd) < 1:
+        await event.edit(LANG['USAGE_PL'])    
 
-CMD_HELP.update(
-    {
-        "song": ">`.song` **atrist title**"
-        "\nUsage: Finding and uploading song.\n"
-        ">`.smd` **<song tittle>**"
-        "\nUsage: **Download music from spotify**"
-    }
-)
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    await event.edit(LANG['SEARCHING_PL'])
+    dosya = os.getcwd() + "/playlist/" + "pl.pl"
+    klasor = os.getcwd() + "/playlist/"
+    sonuc = os.system(f"spotdl --playlist {cmd} --write-to=\"{dosya}\"")
+    sonuc2 = os.system(f"spotdl --list {dosya} -f {klasor}")
+    await event.edit(LANG['DOWNLOADED'])
+    l = glob.glob(f"{klasor}/*.mp3")
+    i = 0
+    if len(l) >= 1:
+        while i < len(l):
+            await event.reply(LANG['SENDING_SONGS'] + l[i])
+            await event.client.send_file(
+                event.chat_id,
+                l[i],
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=reply_to_id
+            )
+    else:
+        await event.edit(LANG['NOT_FOUND_PL'])   
+        return 
+    os.system(f"rm -rf {klasor}/*.mp3")
+    subprocess.check_output(f"rm -rf {klasor}/*.mp3",shell=True)
+    os.system(f"rm -rf {klasor}/*.pl")
+    subprocess.check_output(f"rm -rf {klasor}/*.pl",shell=True)
+
+CmdHelp('song').add_command(
+    'deez', (LANG['DEEZ1']), (LANG['DEEZ2'])
+).add_command(
+    'song', (LANG['SONG1']), (LANG['SONG2'])
+).add_command(
+    'songpl', (LANG['SONG3']), (LANG['SONG4'])
+).add()
